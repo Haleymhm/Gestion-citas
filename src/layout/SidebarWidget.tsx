17f67abe95b2eeb6974286d8fde91ea77f807b70
Haleym Hidalgo Moyetones <haleymhm@gmail.com>
2026-07-30 15:04:24 -0400

    varius

--- src/layout/SidebarWidget.tsx
+++ src/layout/SidebarWidget.tsx
@@ -13,7 +13,7 @@ export default function SidebarWidget() {
         Sistema de gestión de citas veterinarias para clínicas y veterinarios. Administra tus citas, clientes y mascotas de manera eficiente con nuestra plataforma fácil de usar.
       </p>
       <a
-        href="https://tailadmin.com/pricing"
+        href="#"
         target="_blank"
         rel="nofollow"
         className="flex items-center justify-center p-3 font-medium text-white rounded-lg bg-brand-500 text-theme-sm hover:bg-brand-600"
